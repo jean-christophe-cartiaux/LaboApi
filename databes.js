b689e2666d072721f1d00d01010f8e1045a3d89f
Jean-Christophe Cartiaux <cartiaux.jc@gmail.com>
2024-03-26 16:34:29 +0100

    Initial commit

---
+++ databes.js
@@ -0,0 +1,18 @@
+const {DB_USER,DB_PSW,DB_NAME}=process.env;
+
+const sqlConfig={
+    user:DB_USER,
+    password:DB_PSW,
+    database:DB_NAME,
+    server:'localhost',
+    pool:{
+        min:0,
+        max:10,
+        idleTimeoutMilis:300001
+    },
+    options:{
+        trustServerCertificate: true
+    }
+
+}
+module.exports = sqlConfig;
---
+++ middlewares/jwtVerification.js
@@ -0,0 +1,25 @@
+const jwt= require('jsonwebtoken');
+
+const jwtVerification=(req,res,next) =>{
+    const secret = process.env.JWT_SECRET
+
+    const authHeader=req.headers['autorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if(!token){
+        res.sendStatus(401);
+    }else{
+        jwt.verify(token,secret,(err,payload)=>{
+            if(err && err.name !=='TokenExpiredError'){
+                res.sendStatus(403);
+            }else if (err && err.name=== 'TokenExpiredError'){
+                const decodeTokenPayload = jwt.decode(token)
+                const newPayload = {
+                    userId:decodeTokenPayload.userId,
+                    email:decodeTokenPayload.email
+                };
+                const newToken=
+            }
+        })
+    }
+}
