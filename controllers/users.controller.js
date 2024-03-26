ba7b12d4ff0e59f50839b0d25e04564fe6e47b6a
Jean-Christophe Cartiaux <cartiaux.jc@gmail.com>
2024-03-26 16:55:02 +0100

    construction du skelette user.controller

---
+++ controllers/users.controller.js
@@ -0,0 +1,45 @@
+const usersService =require('../services/users.service');
+const commentsService =require('../services/comments.service');
+const favoritsService =require('../services/favorits.service');
+const contenueMessagesService =require('../services/contenuMessages.service');
+const authValidator=require('../validators/auth.validator');
+const jwt =require('jsonwebtoken');
+const bcrypt =require('bcrypt');
+
+const usersController={
+    register:async(req,res)=>{
+        try{
+
+        }catch (err){
+            console.error(err)
+            res.sendStatus(500);
+        }
+    },
+    login:async(req,res)=>{
+        try{
+
+        }catch (err){
+            console.error(err)
+            res.sendStatus(500);
+        }
+    },
+    updateUser:async(req,res)=>{
+        try{
+
+        }catch (err){
+            console.error(err)
+            res.sendStatus(500);
+        }
+    },
+    deleteUser:async(req,res)=>{
+        try{
+
+        }catch (err){
+            console.error(err)
+            res.sendStatus(500);
+        }
+    },
+
+}
+module.exports = usersController;
+
